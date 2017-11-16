125c0d36302a2764776dfdc2e67d5753e4c60574
mischegoss <mischegoss@gmail.com>
2017-11-16 03:37:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-function scuberGreetingForFeet (distance)
-if (distance <=200)
-{
-  control.log ("This one is on me!")
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return 'This one is on me!'
+  } else if ((distance > 2000) && (distance < 2500)){
+    return 'I will gladly take your thirty bucks.'
+  } else if (distance > 2500){
+    return 'No can do.'
+  }
 }
+
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+   return "Ok, sounds good."
+  } else {
+ return "No go."
+   }
+  return (city === "NYC") ? "Ok, sounds good." : "No go."
+}
+
+function switchOnCharmFromTip(tipString){
+  if (tipString === "generous"){
+  return "Thank you so much."
+  } else if (tipString === "not as generous"){
+    return "Thank you."
+ } else if (tipString === "thanks for everything"){
+    return "Bye."
+}
+
+  switch(tipString){
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    case "thanks for everything":
+      return "Bye."
+  }
+}
+
